7a55ed7c071aef64da20fd6640d756f12278e8bc
bruno.chavez <darklaier@gmail.com>
2017-10-28 18:20:23 -0300

    more quotes added.

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/urfave/cli"
 	"encoding/json"
 	"time"
-	//"github.com/bruno-chavez/ancestorquotes"
 	"ancestorquotes/quotes"
 	)
 
--- quotes/quotes.go
+++ quotes/quotes.go
@@ -94,6 +94,183 @@ func Q()[]byte{
   },
   {
     "quote": "Remind yourself that overconfidence is a slow and insidious killer."
+  },
+  {
+    "quote": "There is a place, beneath those ancient ruins, in the moor, that calls out to the boldest among them... 'We are the Flame!', they cry, 'And Darkness fears us!' They descend, spurred on by fantasies of riches and redemption to lay bare whatever blasphemous abnormality may slumber restlessly in that unholy abyss... But Darkness is insidious. Terror and Madness can find cracks in the sturdiest of honors, the most resolute of minds... And below, in that limitless chasm of Chaos, they will realize the truth of it. 'We are not the Flame!', they will cry out, 'We are but moths and we are DOOMED!' And their screams will echo amidst the pitiless cyclopean stones... Of the Darkest Dungeon."
+  },
+  {
+    "quote": "Ruin has come to our family. You remember our venerable house, opulent and imperial. Gazing proudly from its stoic perch above the moor. I lived all my years in that ancient, rumor shadowed manor. Fattened by decadence and luxury. And yet, I began to tire of conventional extravagance. Singular, unsettling tales suggested the mansion itself was a gateway to some fabulous and unnamable power. With relic and ritual, I bent every effort towards the excavation and recovery of those long buried secrets, exhausting what remained of our family fortune on swarthy workmen and sturdy shovels. At last, in the salt-soaked crags beneath the lowest foundations we unearthed that damnable portal and antediluvian evil. Our every step unsettled the ancient earth but we were in a realm of death and madness! In the end, I alone fled laughing and wailing through those blackened arcades of antiquity. Until consciousness failed me. You remember our venerable house, opulent and imperial. It is a festering abomination! I beg you, return home, claim your birthright and deliver our family from the ravenous clutching shadows of the Darkest Dungeon."
+  },
+  {
+    "quote": "You will arrive along the old road. It winds with a troubling, serpent-like suggestion through the corrupted countryside. Leading only, I fear, to ever more tenebrous places. There is a sickness in the ancient pitted cobbles of the old road and on its writhing path you will face viciousness, violence, and perhaps other damnably transcendent terrors. So steel yourself and remember: there can be no bravery without madness. The old road will take you to hell, but in that gaping abyss we will find our redemption."
+  },
+  {
+    "quote": "You answered the letter - now like me, you are part of this place."
+  },
+  {
+    "quote": "We dug for months, years - an eternity. And we were rewarded with madness."
+  },
+  {
+    "quote": "The plume and the pistol - a fitting end to my folly, and a curse upon us all."
+  },
+  {
+    "quote": "Can you feel it? The walls between the sane world and that unplumbed dimension of delirium are tenuously thin here..."
+  },
+  {
+    "quote": "It was a yearning, a thirst for discovery that could be neither numbed, nor sated."
+  },
+  {
+    "quote": "All my life, I could feel an insistent gnawing in the back of my mind."
+  },
+  {
+    "quote": "An eternity of futile struggle - a penance for my unspeakable transgressions."
+  },
+  {
+    "quote": "All the decadent horrors I have seen pale in comparison with that final, crowning thing. I dared not look, nor could I look away!"
+  },
+  {
+    "quote": "Great heroes can be found even here, in the mud and rain."
+  },
+  {
+    "quote": "More arrive, foolishly seeking fortune and glory in this domain of the damned."
+  },
+  {
+    "quote": "Word is travelling. Ambition is stirring in distant cities. We can use this."
+  },
+  {
+    "quote": "With enough ale, maybe they can be inured against the horrors below."
+  },
+  {
+    "quote": "All manner of diversion and dalliance await those who cross the threshold with coin in hand."
+  },
+  {
+    "quote": "Strong drink, a game of chance, and companionship. The rush of life."
+  },
+  {
+    "quote": "A little hope, however desperate, is never without worth."
+  },
+  {
+    "quote": "Gilded icons and dogmatic rituals...for some, a tonic against the bloodshed."
+  },
+  {
+    "quote": "A man in a robe, claiming communion with the divine. Madness."
+  },
+  {
+    "quote": "In the end, every plan relies upon a strong arm, and tempered steel."
+  },
+  {
+    "quote": "A sharper sword, a stronger shield. Anything to prolong a soldier's life."
+  },
+  {
+    "quote": "Fan the flames! Mold the metal! We are raising an army!"
+  },
+  {
+    "quote": "Some may fall, but their knowledge lives on."
+  },
+  {
+    "quote": "Every creature has a weakness. The wise hero trains for what she will face."
+  },
+  {
+    "quote": "A strict regimen is paramount, if one is to master the brutal arithmetic of combat."
+  },
+  {
+    "quote": "Alone in the woods or tunnels, survival is the same. Prepare, persist, and overcome."
+  },
+  {
+    "quote": "Success depends on survival."
+  },
+  {
+    "quote": "They must learn more than brutal bloodletting - they must learn to survive!"
+  },
+  {
+    "quote": "Rarities and curios, sold at a profit, of course."
+  },
+  {
+    "quote": "Idol, amulet, or lucky charm - the simplest object can be a talisman against evil."
+  },
+  {
+    "quote": "An increasing stockpile of curious trinkets, gathered from forbidden places."
+  },
+  {
+    "quote": "The front line of this war is not in the dungeon, but rather, inside the mind."
+  },
+  {
+    "quote": "Experimental techniques and tonics can overcome things a sharpened sword cannot."
+  },
+  {
+    "quote": "Curious methodologies and aperatus' can calm even the most tormented soul."
+  },
+  {
+    "quote": "Tortured and reclusive... this man is more dangerous than he seems..."
+  },
+  {
+    "quote": "She searches where others will not go... and sees what others will not see."
+  },
+  {
+    "quote": "Shoot, bandage and pillage: the dancing steps of war."
+  },
+  {
+    "quote": "The thrill of the hunt... The promise of payment..."
+  },
+  {
+    "quote": "A mighty sword-hand anchored by a holy purpose. A zealous warrior."
+  },
+  {
+    "quote": "To those with the keen eye, gold gleams like a dagger's point."
+  },
+  {
+    "quote": "Barbaric rage and unrelenting savagery make for a powerful ally."
+  },
+  {
+    "quote": "Elusive, evasive, persistent. Righteous traits for a rogue."
+  },
+  {
+    "quote": "A lawman and his faithful beast. A bond forged by battle and bloodshed."
+  },
+  {
+    "quote": "He will be laughing still... at the end."
+  },
+  {
+    "quote": "This man understands that existence and adversity are one and the same."
+  },
+  {
+    "quote": "The raw strength of youth may be spent, but his eyes hold the secrets of a hundred campaigns."
+  },
+  {
+    "quote": "To fight the abyss, one must know it..."
+  },
+  {
+    "quote": "What better laboratory than the blood-soaked battlefield?"
+  },
+  {
+    "quote": "A sister of battle. Pious and unrelenting."
+  },
+  {
+    "quote": "This one has become vestigial, useless."
+  },
+  {
+    "quote": "Suffer not the lame horse... nor the broken man."
+  },
+  {
+    "quote": "Another soul battered and broken, cast aside like a spent torch."
+  },
+  {
+    "quote": "Those without the stomach for this place must move on."
+  },
+  {
+    "quote": "It is done. Turn yourself now to the conditions of those poor devils who remain."
+  },
+  {
+    "quote": "Send this one to journey elsewhere, for we have need of sterner stock."
+  },
+  {
+    "quote": "Slumped shoulders, wild eyes, and a stumbling gait - this one is no more good to us."
+  },
+  {
+    "quote": "The task ahead is terrible, and weakness cannot be tolerated."
+  },
+  {
+    "quote": "Excavations beneath the manor were well underway, when a particular ragged indigent arrived in the hamlet. This filthy, toothless miscreant boasted an uncanny knowledge of my ambitions and prognosticated publicly that left unchecked, I would soon unleash doom upon the world."
   }
 ]`)
 	return quotes
